d571f6a3dda4f84e68cf20c7ab7a8f28225e15e0
Hossein Naderi <mail@h-naderi.ir>
2017-03-23 15:31:13 +0430

    database init script

--- databaseInit.sh
+++ databaseInit.sh
@@ -1,4 +1,4 @@
-mysql -e 'CREATE DATABASE telegraf_sessions;CREATE TABLE `sessions` (
+mysql -u root -e 'CREATE DATABASE telegraf_sessions;CREATE TABLE `sessions` (
   `id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `session` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
